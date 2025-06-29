cec7c91dd8e6f5338e754e22b5a0ac0e2e1c302b
Jaxon Adams <jadamsresume1@gmail.com>
2025-06-29 17:45:47 -0600

    Create a game window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50().with_title("Roguelike").build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
 }
